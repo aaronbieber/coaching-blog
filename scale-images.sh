d90bd7aba93a15283b5910bc929c853b1f989b06
Aaron Bieber <aaron@aaronbieber.com>
2022-11-09 14:07:17 -0500

    Scale PNG images, too

--- scale-images.sh
+++ scale-images.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-for file in `find static/uploads -iname '*jpg'`; do
+for file in `find static/uploads -iname '*jpg' -or -iname '*png'`; do
   if test "`identify $file | awk '{print $3}' | cut -dx -f1`" -gt 1500; then
     convert "$file" -resize '1500x1500>' -verbose "$file"
   fi

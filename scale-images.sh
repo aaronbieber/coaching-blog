c1afef3baac76739a359e771ff3a59bcf0f348c3
Aaron Bieber <aaron@aaronbieber.com>
2025-03-25 08:49:10 -0400

    When scaling images, remove scaled source PNGs

--- scale-images.sh
+++ scale-images.sh
@@ -3,11 +3,13 @@
 for file in `find content/posts -type f -iname '*jpg' -or -iname '*png'`; do
   if test "`identify $file | awk '{print $3}' | cut -dx -f1`" -gt 1500; then
     convert "$file" -resize '1500x1500>' -verbose "${file%.*}.jpg"
+    rm -f "${file%.*}.png"
   fi
 done
 
 for file in `find content/posts -type f -iname '*jpg' -or -iname '*png'`; do
   if test "`ls -l $file | awk '{print $5}'`" -gt 1000000; then
     convert "$file" -resize '1500x1500>' -verbose "${file%.*}.jpg"
+    rm -f "${file%.*}.png"
   fi
 done
